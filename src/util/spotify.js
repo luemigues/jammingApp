2bdd3df3adc574830477f12e98b5249c6a892ce9
Luemigues <lulimgues@hotmail.com>
2021-02-15 14:23:12 +0100

    api redirect_uri update

--- src/util/spotify.js
+++ src/util/spotify.js
@@ -28,7 +28,6 @@ const Spotify = {
     search(term){
 
         const spotifyAccessToken = Spotify.getAccessToken()
-        console.log(accessToken)
 
         return fetch(`https://api.spotify.com/v1/search?type=track&q=${term}`, {
             headers: {Authorization: `Bearer ${spotifyAccessToken}`}
